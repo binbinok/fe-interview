f917c9324bcdb8720a959ee7a3292030109fecc2
Flynn Gao <i@flynngao.com>
2017-10-06 22:41:14 +0800

    fe interview

---
+++ index.js
@@ -0,0 +1,56 @@
+let express = require('express');
+let app = express();
+let path = require('path');
+
+// tempOrders
+let orders = []
+
+function generateOrders() {
+    setInterval(function() {
+        let side = parseInt(Math.random() * 10) % 2 ? 'ask' : 'bid';
+        let price = (50.0 + Math.random() * 180).toFixed(1);
+        let quantity = Math.floor(Math.random() * (15 + 1));
+        let time = new Date().getTime();
+        let obj = {
+            number: orders.length + 1,
+            side,
+            price,
+            quantity,
+            time
+        };
+
+        orders.push(obj);
+    }, 1000)
+}
+
+generateOrders();
+// static router
+
+app.use("/example", function(req, res){
+    res.sendFile(path.join(__dirname+'/static/example.html'));
+})
+
+// Get Orders List
+app.use("/api/getOrders", function(req, res) {
+    let start = req.query.start || (orders.length - 20 < 0 ? 0 : orders.length - 20);
+    let size = req.query.size || 20;
+
+    if(start < 0 || size < 1){
+        res.send('request params error');
+        return;
+    }
+    let end = start + size;
+    res.json(orders.slice(start, end));
+
+})
+
+// reset Orders
+app.use('/api/reset', function(req, res) {
+    orders = [];
+    res.send('Orders has reseted')
+})
+
+
+app.listen(8080, function(){
+    console.log('Start Fe Interview Service on 8080')
+})
